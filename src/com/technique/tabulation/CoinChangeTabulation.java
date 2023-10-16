fef2bcb39d0df1d4706ca930811df394b0881e7f
imran-dev100 <iagro7189@gmail.com>
2023-10-16 21:25:43 +0530

    Adding comments

--- src/com/technique/tabulation/CoinChangeTabulation.java
+++ src/com/technique/tabulation/CoinChangeTabulation.java
@@ -7,15 +7,23 @@ public class CoinChangeTabulation {
 	private static int coinChange(int[] coins, int amount) {
 
 		int init = amount + 1;
+
+		// Initializing cache of 1 size greater than amount
 		int[] cache = new int[init];
+
+		// Populate cache with 1 size greater than amount
 		Arrays.fill(cache, init);
+
+		// Fill index 0 of cache with value 0
 		cache[0] = 0;
 
+		// Populate cache
 		for (int i = 1; i <= amount; i++)
 			for (int coin : coins)
 				if (i - coin >= 0)
 					cache[i] = Math.min(cache[i], cache[i - coin] + 1);
 
+		// Check if amount index does not have initial value
 		return cache[amount] == init ? -1 : cache[amount];
 	}
 
