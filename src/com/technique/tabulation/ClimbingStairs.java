674d466d462474a52b54eb4c4b730e5531aef711
imran-dev100 <111573254+imran-dev100@users.noreply.github.com>
2023-10-17 20:11:16 +0530

    Create ClimbingStairs.java

---
+++ src/com/technique/tabulation/ClimbingStairs.java
@@ -0,0 +1,18 @@
+package com.technique.tabulation;
+
+/**
+ *
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ */
+public class ClimbingStairs {
+
+	public static void main(String[] args) {
+		System.out.print(climbingStairs2D(45));
+	}
+
+	private static Integer climbingStairs2D(int i) {
+
+		return null;
+	}
+}
