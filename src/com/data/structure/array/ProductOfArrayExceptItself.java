f761748c4e3a8cb18a5893319f0c4e3ab295c281
Imran Ansari <iagro7189@gmail.com>
2023-11-01 16:57:53 +0530

    Array

---
+++ src/com/data/structure/array/ProductOfArrayExceptItself.java
@@ -0,0 +1,49 @@
+package com.data.structure.array;
+
+import java.util.Arrays;
+
+public class ProductOfArrayExceptItself {
+
+	public static void main(String[] args) {
+
+		System.out.println("Input Array");
+		int[] arr = { 1, 2, 3, 4 };
+		printArray(arr);
+
+		System.out.println("Result Array");
+		printArray(getProductArray(arr));
+	}
+
+	private static void printArray(int[] arr) {
+		for (int i : arr)
+			System.out.print(i + " ");
+		System.out.println();
+	}
+
+	private static int[] getProductArray(int[] arr) {
+		int n = arr.length;
+		int[] prod = new int[n];
+
+		int i = 0;
+		int temp = 1;
+
+		for (i = 0; i < n; i++)
+			prod[i] = temp;
+
+		temp = 1;
+
+		for (i = 0; i < n; i++) {
+			prod[i] = temp;
+			temp = temp * arr[i];
+		}
+
+		temp = 1;
+
+		for (i = n - 1; i >= 0; i--) {
+			prod[i] = prod[i] * temp;
+			temp = temp * arr[i];
+		}
+
+		return prod;
+	}
+}
