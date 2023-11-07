53984ad3f9983df6a0dedf03a20144518be7b6d6
imran-dev100 <111573254+imran-dev100@users.noreply.github.com>
2023-11-07 21:01:16 +0530

    Update CountingBits.java

--- src/com/technique/operations/binary/CountingBits.java
+++ src/com/technique/operations/binary/CountingBits.java
@@ -1,1 +1,20 @@
+package com.technique.operations.binary;
 
+public class CountingBits {
+
+  public static void main(String[] args) {
+		int[] arr  = countBits(5);
+    
+      
+	}
+
+  public int[] countBits(int n) {
+        
+        int[] ans = new int[n+1];
+
+        for (int i = 1; i <= n; ++i)
+            ans[i] = ans[i / 2] + (i % 2 == 0 ? 0 : 1);
+
+        return ans;
+  }
+}
