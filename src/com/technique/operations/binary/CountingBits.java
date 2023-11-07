5ac4d5a1f47aebe0f5298631cda7c21b4c893115
imran-dev100 <111573254+imran-dev100@users.noreply.github.com>
2023-11-07 21:02:31 +0530

    Update CountingBits.java

--- src/com/technique/operations/binary/CountingBits.java
+++ src/com/technique/operations/binary/CountingBits.java
@@ -1,12 +1,13 @@
 package com.technique.operations.binary;
 
 public class CountingBits {
-
+//Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+	
   public static void main(String[] args) {
-		int[] arr  = countBits(5);
+	int[] arr  = countBits(5);
 	  for(int i:arr)
 		System.out.print(i+" ");
-	}
+  }
 
   public int[] countBits(int n) {
         
