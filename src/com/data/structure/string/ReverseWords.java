c8824c47b83f16f41a8c809d474ddb9095473853
Imran Ansari <iagro7189@gmail.com>
2023-11-14 18:55:08 +0530

    ReverseWords

--- src/com/data/structure/string/ReverseWords.java
+++ src/com/data/structure/string/ReverseWords.java
@@ -3,7 +3,9 @@ package com.data.structure.string;
 public class ReverseWords {
 
 	public static void main(String[] args) {
-
+		String s = "i love programming very much";
+		System.out.println("Original String ".concat(s));
+		System.out.println("Reversed String ".concat(reverseWords(s)));
 	}
 
 	private static String reverseWords(String s) {
