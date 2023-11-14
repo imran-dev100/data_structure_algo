d3e2f31219139d360bc54bc832e1021d0883bda0
Imran Ansari <iagro7189@gmail.com>
2023-11-14 18:59:11 +0530

    ReverseWords

--- src/com/data/structure/string/ReverseWords.java
+++ src/com/data/structure/string/ReverseWords.java
@@ -1,16 +1,31 @@
 package com.data.structure.string;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class ReverseWords {
 
 	public static void main(String[] args) {
 		String s = "i love programming very much";
-		System.out.println("Original String ".concat(s));
-		System.out.println("Reversed String ".concat(reverseWords(s)));
+		System.out.println("Original String: ".concat(s));
+		System.out.println("Reversed String: ".concat(reverseWords(s)));
 	}
 
 	private static String reverseWords(String s) {
+		
+		String[] arr = s.split(" ");
+		//Using stack
+		Deque<String> stack = new ArrayDeque<>();
+		
+		for(String a:arr)
+			stack.push(a);
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while(!stack.isEmpty())
+			sb.append(stack.pop()+" ");
 	
-		return s;
+		return sb.toString();
 	}
 	
 }
