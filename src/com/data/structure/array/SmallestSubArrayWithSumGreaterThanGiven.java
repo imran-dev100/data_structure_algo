c7da940c349919b53efcb38c01a39bf71d1d506a
Imran Ansari <iagro7189@gmail.com>
2023-11-16 20:14:46 +0530

    Brute force

--- src/com/data/structure/array/SmallestSubArrayWithSumGreaterThanGiven.java
+++ src/com/data/structure/array/SmallestSubArrayWithSumGreaterThanGiven.java
@@ -1,15 +1,38 @@
 package com.data.structure.array;
 
 public class SmallestSubArrayWithSumGreaterThanGiven {
-	
+
 	public static void main(String[] args) {
-		int[] arr = {1, 4, 45, 6, 0, 19};
+		int[] arr = { 1, 4, 45, 6, 0, 19 };
 		int x = 51;
-		getSmallestSubArrayWithSumGreaterThanGiven(arr, x);
+		System.out.println(getSmallestSubArrayWithSumGreaterThanGiven(arr, x));
+
+		arr = new int[] { 1, 10, 5, 2, 7 };
+		x = 9;
+		System.out.println(getSmallestSubArrayWithSumGreaterThanGiven(arr, x));
+
 	}
-	
-	private static void getSmallestSubArrayWithSumGreaterThanGiven(int[] arr,int x) {
-		
+
+	private static int getSmallestSubArrayWithSumGreaterThanGiven(int[] arr, int x) {
+		int n = arr.length;
+		int min = Integer.MAX_VALUE;
+
+		for (int i = 0; i < n; i++) {
+			int sum = arr[i];
+			int c = 1;
+			if (sum > x)
+				return c;
+			for (int j = i + 1; j < n; j++) {
+				sum += arr[j];
+				c += 1;
+				if (sum > x && c < min) {
+					min = c;
+					break;
+				}
+			}
+		}
+
+		return min;
 	}
 
 }
