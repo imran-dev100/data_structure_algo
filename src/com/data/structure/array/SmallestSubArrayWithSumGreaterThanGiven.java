2c7741591f9a3ff035f65208ed5accc5a557a3eb
Imran Ansari <iagro7189@gmail.com>
2023-11-16 19:58:12 +0530

    SmallestSubArrayWithSumGreaterThanGiven

--- src/com/data/structure/array/SmallestSubArrayWithSumGreaterThanGiven.java
+++ src/com/data/structure/array/SmallestSubArrayWithSumGreaterThanGiven.java
@@ -3,6 +3,12 @@ package com.data.structure.array;
 public class SmallestSubArrayWithSumGreaterThanGiven {
 	
 	public static void main(String[] args) {
+		int[] arr = {1, 4, 45, 6, 0, 19};
+		int x = 51;
+		getSmallestSubArrayWithSumGreaterThanGiven(arr, x);
+	}
+	
+	private static void getSmallestSubArrayWithSumGreaterThanGiven(int[] arr,int x) {
 		
 	}
 
