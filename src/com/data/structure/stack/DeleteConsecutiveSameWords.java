e2a6ffb6db497816891d944b7e7b0eefe99685de
Imran Ansari <iagro7189@gmail.com>
2023-11-20 19:33:32 +0530

    Merge branch 'main' of https://github.com/imran-dev100/data_structure_algo.git into main

---
+++ src/com/data/structure/stack/DeleteConsecutiveSameWords.java
@@ -0,0 +1,29 @@
+package com.data.structure.stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class DeleteConsecutiveSameWords {
+
+	public static void main(String[] args) {
+		String s = "ab aa aa bcd ab";
+		System.out.println(deleteConsecutiveWords(s));
+		s = "cn tom jerry jerry tom cn";
+		System.out.println(deleteConsecutiveWords(s));
+	}
+
+	private static int deleteConsecutiveWords(String s) {
+
+		final String[] ar = s.split(" ");
+		final Deque<String> stack = new ArrayDeque<>();
+
+		for (final String a : ar) {
+			if (!stack.isEmpty() && stack.peek().equals(a))
+				stack.pop();
+			else
+				stack.push(a);
+		}
+
+		return stack.size();
+	}
+}
