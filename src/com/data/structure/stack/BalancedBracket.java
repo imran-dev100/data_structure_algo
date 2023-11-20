7f663334f82e7ccc0d632d367fb48484a28eadd5
Imran Ansari <iagro7189@gmail.com>
2023-11-20 20:05:42 +0530

    BalancedBracket

---
+++ src/com/data/structure/stack/BalancedBracket.java
@@ -0,0 +1,51 @@
+package com.data.structure.stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class BalancedBracket {
+
+	public static void main(String[] args) {
+
+		System.out.println(isBalanced("{({[]})}"));
+		System.out.println(isBalanced("{()[]}"));
+		System.out.println(isBalanced("[()]{}{[()()]()}"));
+		System.out.println(isBalanced("[(])"));
+
+	}
+
+	private static boolean isBalanced(String s) {
+
+		final int n = s.length();
+		final Deque<Character> stack = new ArrayDeque<>();
+
+		for (int i = 0; i < n; i++) {
+
+			final char c = s.charAt(i);
+
+			if (c == '{' || c == '[' || c == '(')
+				stack.push(c);
+			else {
+
+				switch (c) {
+
+				case '}':
+					if (stack.peek() == '[' || stack.peek() == '(')
+						return false;
+					break;
+				case ')':
+					if (stack.peek() == '[' || stack.peek() == '{')
+						return false;
+					break;
+				case ']':
+					if (stack.peek() == '(' || stack.peek() == '{')
+						return false;
+					break;
+				}
+				stack.pop();
+			}
+		}
+
+		return true;
+	}
+}
