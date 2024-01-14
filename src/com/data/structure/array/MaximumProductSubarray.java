2b763362c5c495d671208420dbc9e9901d214c85
imran-dev100 <111573254+imran-dev100@users.noreply.github.com>
2024-01-14 19:31:42 +0530

    Update MaximumProductSubarray.java

--- src/com/data/structure/array/MaximumProductSubarray.java
+++ src/com/data/structure/array/MaximumProductSubarray.java
@@ -8,7 +8,6 @@ public class MaximumProductSubarray {
 	 * 
 	 * The test cases are generated so that the answer will fit in a 32-bit integer.
 	 * 
-	 * 
 	 * Example 1:
 	 * 
 	 * Input: nums = [2,3,-2,4] Output: 6 Explanation: [2,3] has the largest product
