71650871c42a57fdfd0e6c1a7ebded93eb56f493
imran-dev100 <111573254+imran-dev100@users.noreply.github.com>
2024-01-17 16:14:35 +0530

    Update MaximumProductSubarray.java

--- src/com/data/structure/array/MaximumProductSubarray.java
+++ src/com/data/structure/array/MaximumProductSubarray.java
@@ -12,14 +12,12 @@ public class MaximumProductSubarray {
 	 * 
 	 * Input: nums = [2,3,-2,4] Output: 6 Explanation: [2,3] has the largest product
 	 * 6.
-	 * 
 	 * Example 2:
 	 * 
 	 * Input: nums = [-2,0,-1] Output: 0 Explanation: The result cannot be 2,
 	 * because [-2,-1] is not a subarray.
 	 * 
 	 * Constraints:
-	 * 
 	 * 1 <= nums.length <= 2 * 104 -10 <= nums[i] <= 10 The product of any prefix or
 	 * suffix of nums is guaranteed to fit in a 32-bit integer.
 	 * 
