36d56b31db530f59fe9daf062d038adaa9b8e842
Imran Ansari <iagro7189@gmail.com>
2024-01-23 22:02:28 +0530

    Streams API demo

---
+++ src/com/streams/General.java
@@ -0,0 +1,47 @@
+package com.streams;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class General {
+	private static final String ORIGINAL_LIST = "Original List";
+
+	public static void main(String[] args) {
+
+		List<Integer> list = Arrays.asList(8, 6, 7, 4, 5, 9, 3, 1, 2);
+		System.out.println(ORIGINAL_LIST);
+		System.out.println(printListUsingStreams(list));
+		System.out.println("Average of the numbers: " + calculateAverage(list));
+
+		list = Arrays.asList(8, 9, 6, 7, 3, 4, 5, 8, 9, 3, 1, 6, 2, 4);
+		System.out.println(ORIGINAL_LIST);
+		System.out.println(printListUsingStreams(list));
+		System.out.println("Distinct numbers of the list: \n" + getDistinctAndSortedInDescending(list));
+		
+		list = Arrays.asList(8, 6, 7, 4, 5, 9, 3, 1, 2);
+		System.out.println(ORIGINAL_LIST);
+		System.out.println(printListUsingStreams(list));
+		System.out.println("Fourth largest number of the list: \n" + getFourthLargestNumber(list));
+
+	}
+
+	private static String printListUsingStreams(final List<Integer> list) {
+		return list.stream().map(String::valueOf).reduce((int1, int2) -> int1 + " " + int2).orElse(Collections.emptyList().toString());
+	}
+
+	private static Integer getFourthLargestNumber(final List<Integer> list) {
+		return list.stream().sorted(Comparator.reverseOrder()).skip(3L).findFirst().orElse(null);
+	}
+
+	private static List<Integer> getDistinctAndSortedInDescending(final List<Integer> list) {
+//		return list.stream().distinct().sorted((a, b) -> Integer.compare(b, a)).toList();
+//		OR
+		return list.stream().distinct().sorted(Comparator.reverseOrder()).toList();
+	}
+
+	private static double calculateAverage(final List<Integer> list) {
+		return list.stream().mapToDouble(Integer::doubleValue).average().orElse(0.0);
+	}
+}
