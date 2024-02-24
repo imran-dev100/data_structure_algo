2120d51738f81dbdfaf424e22e7bc15c56d7a717
imran-dev100 <111573254+imran-dev100@users.noreply.github.com>
2024-02-24 12:10:11 +0530

    Update ArrayContainsDuplicate.java

--- src/com/data/structure/array/ArrayContainsDuplicate.java
+++ src/com/data/structure/array/ArrayContainsDuplicate.java
@@ -4,11 +4,12 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ArrayContainsDuplicate {
-
+    
     public static void main(String[] args) {
         final int[] ar = {1,2,3,4};
         System.out.print(containsDuplicate(ar));
     }
+
     private static boolean containsDuplicate(int[] ar){
         final Set<Integer> set = new HashSet<>();
         for (int i:ar) {
