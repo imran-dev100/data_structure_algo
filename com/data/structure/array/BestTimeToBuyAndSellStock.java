ab1d1bf2f5362dd2dd21b1a3d1786515ce80e749
imran-dev100 <111573254+imran-dev100@users.noreply.github.com>
2024-03-19 08:02:49 +0530

    Update BestTimeToBuyAndSellStock.java

--- com/data/structure/array/BestTimeToBuyAndSellStock.java
+++ com/data/structure/array/BestTimeToBuyAndSellStock.java
@@ -5,7 +5,6 @@ public class BestTimeToBuyAndSellStock {
 //    You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 //    Return the maximum profit you can achieve from this transaction. 
 	public static void main(String[] args) {
-
 		int[] prices = new int[] { 7, 1, 5, 3, 6, 4 };
 //		System.out.println(maxProfit(prices));
 		bestTime(prices);
